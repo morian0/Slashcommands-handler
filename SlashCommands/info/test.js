e3106ae6fe659ec02165dcb990cfec658e6e3e40
Moustafa <100532044+morian0@users.noreply.github.com>
2022-04-24 01:14:55 +0300

    Update test.js

--- SlashCommands/info/test.js
+++ SlashCommands/info/test.js
@@ -1,26 +1,12 @@
-const { Modal, TextInputComponent, showModal } = require('discord-modals') // Now we extract the showModal method
-const { Formatters } = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 module.exports = {
-    name: 'test',
-    description: 'test discord-modal',
-    cooldown: 5,
+  name: 'test',
+  description: 'test command',
+  cooldown: 7,
+  run: async function(client, interaction, args) {
 
-    run: async function(client, interaction, modal ) {
+let embed = new MessageEmbed()
+.setTitle("By: Moriano")
 
-const modal2 = new Modal() // We create a Modal
-.setCustomId('modal-customid')
-.setTitle('Test of Discord-Modals!')
-.addComponents([
-  new TextInputComponent() // We create a Text Input Component
-  .setCustomId('textinput-customid')
-  .setLabel('Some text Here')
-  .setStyle('SHORT') //IMPORTANT: Text Input Component Style can be 'SHORT' or 'LONG'
-  .setMinLength(4)
-  .setMaxLength(10)
-  .setPlaceholder('Write a text here')
-  .setRequired(true) // If it's required or not
-]);
-          if(modal.customId === 'modal-customid'){
-            const firstResponse = modal2.getTextInputValue('textinput-customid')
-            modal.followUp({ content: 'done yasta' + Formatters.codeBlock('markdown', firstResponse)})
-          }}};
+interaction.followUp({ embeds: [embed] })
+  }}
